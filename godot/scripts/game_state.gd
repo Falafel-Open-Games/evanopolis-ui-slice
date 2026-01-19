c2c28f8dea177330ae1d295c6098870923b00b56
Fabricio C Zuardi <fabricio@fabricio.org>
2026-01-19 17:47:25 -0300

    feat: add tile metadata and pawn movement

--- godot/scripts/game_state.gd
+++ godot/scripts/game_state.gd
@@ -9,5 +9,37 @@ extends Node
 
 signal player_changed(new_index: int)
 
+var player_positions: Array[int] = []
+var tile_occupants: Dictionary = {}
+
+func reset_positions() -> void:
+	player_positions = []
+	tile_occupants = {}
+	for index in range(player_count):
+		player_positions.append(0)
+	tile_occupants[0] = []
+	for index in range(player_count):
+		tile_occupants[0].append(index)
+
 func advance_turn() -> void:
 	current_player_index = (current_player_index + 1) % player_count
+
+func move_player(player_index: int, steps: int, board_size: int) -> Dictionary:
+	var result: Dictionary = {}
+	if player_index < 0 or player_index >= player_positions.size():
+		return result
+
+	var current_tile: int = player_positions[player_index]
+	var next_tile: int = (current_tile + steps) % board_size
+
+	if tile_occupants.has(current_tile):
+		tile_occupants[current_tile].erase(player_index)
+
+	if not tile_occupants.has(next_tile):
+		tile_occupants[next_tile] = []
+	tile_occupants[next_tile].append(player_index)
+	player_positions[player_index] = next_tile
+
+	result["tile_index"] = next_tile
+	result["slot_index"] = tile_occupants[next_tile].size() - 1
+	return result
