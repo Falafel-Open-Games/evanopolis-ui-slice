f9d25c4f5bde5de58f1de5a202169a33be4c9cbe
Fabricio C Zuardi <fabricio@fabricio.org>
2026-01-22 10:28:23 -0300

    feat: display empty property texture on top of all property tiles

--- godot/scripts/board_tile.gd
+++ godot/scripts/board_tile.gd
@@ -3,6 +3,9 @@ class_name BoardTile
 extends Node3D
 
 var _tile_color: Color = Color.WHITE
+var _color_material: StandardMaterial3D = StandardMaterial3D.new()
+var _top_material: Material
+var _bottom_material: Material
 
 @export var tile_color: Color = Color.WHITE:
 	get:
@@ -12,12 +15,44 @@ var _tile_color: Color = Color.WHITE
 		if is_inside_tree():
 			_apply_color()
 
+@export var top_face: MeshInstance3D
+@export var bottom_face: MeshInstance3D
+@export var side_mesh: MeshInstance3D
+
+@export var top_material: Material:
+	get:
+		return _top_material
+	set(value):
+		_top_material = value
+		if is_inside_tree():
+			_apply_face_materials()
+
+@export var bottom_material: Material:
+	get:
+		return _bottom_material
+	set(value):
+		_bottom_material = value
+		if is_inside_tree():
+			_apply_face_materials()
+
 func _ready() -> void:
+	assert(top_face)
+	assert(bottom_face)
+	assert(side_mesh)
 	_apply_color()
+	_apply_face_materials()
 
 func _apply_color() -> void:
-	var material: StandardMaterial3D = StandardMaterial3D.new()
-	material.albedo_color = _tile_color
-	for mesh in find_children("*", "MeshInstance3D", true, false):
-		if mesh is MeshInstance3D:
-			mesh.material_override = material
+	_color_material.albedo_color = _tile_color
+	side_mesh.material_override = _color_material
+	_apply_face_materials()
+
+func _apply_face_materials() -> void:
+	var top_override: Material = _top_material
+	var bottom_override: Material = _bottom_material
+	if top_override == null:
+		top_override = _color_material
+	if bottom_override == null:
+		bottom_override = _color_material
+	top_face.set_surface_override_material(0, top_override)
+	bottom_face.set_surface_override_material(0, bottom_override)
